1af8451fc5baa6f066accf1a701e8e8742366ebe
Aziz Abbas <aziz@builder.io>
2022-02-09 15:36:46 -0800

    lower validation time

--- pages/[[...path]].tsx
+++ pages/[[...path]].tsx
@@ -44,7 +44,7 @@ export async function getStaticProps({
     // Next.js will attempt to re-generate the page:
     // - When a request comes in
     // - At most once every 30 seconds
-    revalidate: 30,
+    revalidate: 5,
   }
 }
 
