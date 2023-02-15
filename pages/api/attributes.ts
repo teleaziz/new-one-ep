35be2d30f48a63f7bc95200d9434dbb14698fcb9
Aziz Abbas <aziz@builder.io>
2023-02-15 15:58:19 -0500

    update

--- pages/api/attributes.ts
+++ pages/api/attributes.ts
@@ -1,9 +1,18 @@
-import { getAttributes } from '@builder.io/personalization-utils/dist/get-attributes'
 import { NextApiRequest, NextApiResponse } from 'next'
+import { createAdminApiClient } from '@builder.io/admin-sdk';
 
 if (!process.env.BUILDER_PRIVATE_KEY) {
   throw new Error('No BUILDER_PRIVATE_KEY defined')
 }
+
+export const getAttributes = async (privateKey: string) => {
+  const adminSDK = createAdminApiClient(privateKey);
+  const res = await adminSDK.query({
+    settings: true,
+  })
+  return res.data?.settings.customTargetingAttributes;
+};
+
 
 /**
  * API to get the custom targeting attributes from Builder, only needed for the context menu to show a configurator and allow toggling of attributes
